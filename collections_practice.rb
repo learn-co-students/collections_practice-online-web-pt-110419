201b99022390ce104bdbba2c76189062800f8ccc
Brandon Weaver <a7xncob@gmail.com>
2019-11-06 04:08:00 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,60 @@
+def sort_array_asc(array)
+  return array.sort
+end
 
+def sort_array_desc(array)
+  return array.sort.reverse
+end
+
+def sort_array_char_count(strings)
+  strings.sort do |a, b|
+    a.size <=> b.size
+  end
+end
+
+def swap_elements(array)
+  temp = array[2]
+  array[2] = array[1]
+  array[1] = temp
+  return array
+end
+
+def reverse_array(array)
+  return array.reverse
+end
+
+def kesha_maker(strings)
+  strings.each do |string|
+    string[2] = '$'
+  end
+  return strings
+end
+
+def find_a(strings)
+  temp = []
+  strings.collect do |string|
+    if string.start_with?('a')
+      temp << string
+    end
+  end
+  return temp
+end
+
+def sum_array(array)
+  sum = 0
+  array.each do |number|
+    sum += number
+  end
+  return sum
+end
+
+def add_s(strings)
+  index = 0
+  while index < strings.size do
+    if index != 1
+      strings[index] = "#{strings[index]}s"
+    end
+    index += 1
+  end
+  return strings
+end
