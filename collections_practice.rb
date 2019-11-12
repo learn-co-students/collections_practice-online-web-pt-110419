87bf1dd78e9d3f4b491a7772a872300e504dc0c0
Daniel Reina <dannyflatiron@gmail.com>
2019-11-12 01:37:09 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,48 @@
+require 'pry'
+def sort_array_asc(array)
+  array.sort do |a, b|
+    a <=> b
+  end
+end
 
+def sort_array_desc(array)
+  array.sort! do |a, b|
+    b <=> a
+  end
+end
+
+def sort_array_char_count(array)
+  array.sort_by {|d| d.length}
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+  #array needs to be explicitly mentioned because the test is asking for the value of the entire array not just two indexes
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.each do |n|
+    n[2] = "$"
+  end
+  # binding.pry
+end
+
+def find_a(array)
+  array.select {|n| n.start_with? "a"}
+  # binding.pry
+end
+
+def sum_array(array)
+  array.inject(:+)
+end
+
+def add_s(array)
+  array.each_with_index.collect {|element, index| index == 1 ? element : element << "s"}
+
+  # binding.pry
+end
