7e4ddd77e44a2cd6f8cf8dc8e2022d8ac29018b9
Calvin McDowell <calvinmac40@hotmail.com>
2019-11-17 10:08:19 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,44 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort{|a,b| b <=> a}
+end
+
+def sort_array_char_count(strings)
+  strings.sort{|a,b|
+  a.length <=> b.length}
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(array)
+  array.reverse!
+  array
+end
+
+def kesha_maker(array)
+  array.each{|n| n[2] = "$"}
+end
+
+def find_a(array)
+  array.select{|n| n.start_with?("a")}
+end
+
+def sum_array(array)
+  #array.inject(0){|sum,num| sum + num}
+  array.sum 
+end
+
+def add_s(array)
+  array.collect do |n|
+    if array[1] == n
+      n
+    else n + "s"
+  end
+end
+end
